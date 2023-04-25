18fab9b8c4e76d6225eaa524c8cf660e20fd8254
BorjaMontoro <bmontoroplaza.cf@iesesteveterradas.cat>
2023-04-25 19:10:06 +0200

    Añadidas funciones get ciclos y familias

--- app.js
+++ app.js
@@ -8,18 +8,6 @@ const wait        = require('./utilsWait.js')
 
 var db = new database()   // Database example: await db.query("SELECT * FROM test")
 var ws = new webSockets()
-var jugadors = 0
-var player1;
-var player2;
-var points1=0;
-var points2=0;
-const fps = 60
-const ballSpeed=200
-let ballX = Number.POSITIVE_INFINITY
-let ballY = Number.POSITIVE_INFINITY
-const ballSize = 15
-const ballHalf = ballSize/2
-let ballDirection = "upRight"
 
 // Start HTTP server
 const app = express()
@@ -30,7 +18,7 @@ app.use(express.static('public'))
 
 // Activate HTTP server
 const httpServer = app.listen(port, appListen)
-console.log(httpServer);
+//console.log(httpServer);
 function appListen () {
   console.log(`Listening for HTTP queries on: http://localhost:${port}`)
 }
@@ -52,6 +40,73 @@ db.init({
   port: process.env.MYSQLPORT || 3306,
   user: process.env.MYSQLUSER || "root",
   password: process.env.MYSQLPASSWORD || "",
-  database: process.env.MYSQLDATABASE || "test"
+  database: process.env.MYSQLDATABASE || "IPOP"
 })
 ws.init(httpServer, port, db)
+
+
+// Define routes
+app.post('/get_families_professionals', getFamilies)
+async function getFamilies (req, res) {
+
+  let receivedPOST = await post.getPostObject(req)
+  let result = { status: "ERROR", message: "Unkown type" }
+
+  if (receivedPOST) {
+    const families = await db.query("select nom from Families_Professionals");
+    const nombreFamilies = families.map(item => item.nom);
+    result = {status: "OK", message: "Les families",families: nombreFamilies}
+    }
+
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(result))
+}
+
+// Define routes
+app.post('/get_cicles', getCicles)
+async function getCicles (req, res) {
+
+  let receivedPOST = await post.getPostObject(req)
+  let result = { status: "ERROR", message: "Unkown type" }
+
+  if (receivedPOST) {
+    const cont = await db.query("select count(*) as cont from Families_Professionals where nom='"+receivedPOST.nom+"'");
+    if(cont[0]["cont"]>0){
+      const id = await db.query("select id from Families_Professionals where nom='"+receivedPOST.nom+"'");
+      const ciclos = await db.query("select nom from Cicles where id_familia_professional="+id[0]["id"]+"");
+      const nombreCiclos = ciclos.map(item => item.nom);
+
+      result = {status: "OK", message: "Si existeix la familia",ciclos: nombreCiclos}
+    }else{
+      result = {status: "ERROR", message: "No existeix la familia indicada"}
+    }
+  }
+
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(result))
+}
+
+// Define routes
+app.post('/set_record', setRecord)
+async function setRecord (req, res) {
+
+  let receivedPOST = await post.getPostObject(req)
+  let result = { status: "ERROR", message: "Unkown type" }
+
+  if (receivedPOST) {
+    const cont = await db.query("select count(*) as cont from Cicles where nom='"+receivedPOST.cicle+"'");
+    if(cont[0]["cont"]>0){
+      // const id = await db.query("select id from Families_Professionals where nom='"+receivedPOST.nom+"'");
+      // const ciclos = await db.query("select nom from Cicles where id_familia_professional="+id[0]["id"]+"");
+      // const nombreCiclos = ciclos.map(item => item.nom);
+      await db.query("insert into Ranking(alies,cicle, puntuacio, temps, encerts, errades) values('"+ receivedPOST.alies+"','"+ receivedPOST.name+"', 18, '"+ receivedPOST.temps +"', "+ receivedPOST.encerts +", '"+receivedPOST.errades+"');");
+
+      result = {status: "OK", message: "Si existeix el cicle"}
+    }else{
+      result = {status: "ERROR", message: "No existeix el cicle indicat"}
+    }
+  }
+
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(result))
+}
