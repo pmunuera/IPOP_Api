9b41bd0a281c88480d5be4657bb0bc7a54c624fd
pmunuera <95918506+pmunuera@users.noreply.github.com>
2023-04-27 16:19:43 +0200

    Algoritmo puntuacion

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static('public'))
 // Activate HTTP server
 const httpServer = app.listen(port, appListen)
 //console.log(httpServer);
-function appListen () {
+async function appListen () {
   console.log(`Listening for HTTP queries on: http://localhost:${port}`)
 }
 
@@ -36,11 +36,11 @@ function shutDown() {
 
 // Init objects
 db.init({
-  host: process.env.MYSQLHOST || "localhost",
-  port: process.env.MYSQLPORT || 3306,
+  host: process.env.MYSQLHOST || "containers-us-west-168.railway.app",
+  port: process.env.MYSQLPORT || 5657,
   user: process.env.MYSQLUSER || "root",
-  password: process.env.MYSQLPASSWORD || "",
-  database: process.env.MYSQLDATABASE || "IPOP"
+  password: process.env.MYSQLPASSWORD || "2nhSo0brHkHt78S9xV12",
+  database: process.env.MYSQLDATABASE || "railway"
 })
 ws.init(httpServer, port, db)
 
@@ -97,7 +97,20 @@ async function setRecord (req, res) {
     const cont = await db.query("select count(*) as cont from Cicles where nom='"+receivedPOST.cicle+"'");
     if(cont[0]["cont"]>0){
       const id = await db.query("select id from Cicles where nom='"+receivedPOST.cicle+"'");
-      let puntuacion=receivedPOST.encerts-receivedPOST.errades;
+      var tempsLimit = 600;
+      let puntuacion=0;
+      let [horas, minutos, segundos] = receivedPOST.temps.split(":");
+      let segundosTotales = parseInt(horas) * 3600 + parseInt(minutos) * 60 + parseInt(segundos);
+      let segundosRestantes=tempsLimit-segundosTotales;
+      if(segundosTotales<tempsLimit){
+        puntuacion=(receivedPOST.encerts*(150+(segundosRestantes)))-(receivedPOST.errades*Math.round((segundosTotales)/4));
+      }
+      else{
+        puntuacion=(receivedPOST.encerts*150)-(receivedPOST.errades*Math.round(tempsLimit/4));
+      }
+      if(puntuacion<0){
+        puntuacion=0;
+      }
       await db.query("insert into Ranking(alies,cicle, puntuacio, temps, encerts, errades) values('"+ receivedPOST.alies+"',"+ id[0]["id"]+", "+puntuacion+", '"+ receivedPOST.temps +"', "+ receivedPOST.encerts +", "+receivedPOST.errades+");");
 
       result = {status: "OK", message: "S'ha afegit el record"}
@@ -106,6 +119,23 @@ async function setRecord (req, res) {
     }
   }
 
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(result))
+}
+app.post('/get_ranking',getRanking)
+async function getRanking(req,res){
+  let receivedPOST = await post.getPostObject(req)
+  let result = { status: "ERROR", message: "Unkown type" }
+  let ranking = [];
+  if(receivedPOST){
+    if(receivedPOST.limitElements<=20){
+      ranking = await db.query("SELECT * FROM Ranking ORDER BY Puntuacio desc LIMIT "+receivedPOST.limitInici+", "+receivedPOST.limitElements+";")
+    }
+    else if(receivedPOST.limitElements>20){
+      ranking = await db.query("SELECT * FROM Ranking ORDER BY Puntuacio desc LIMIT "+receivedPOST.limitInici+", 20;")
+    }
+    result = {status: "OK",ranking:ranking}
+  }
   res.writeHead(200, { 'Content-Type': 'application/json' })
   res.end(JSON.stringify(result))
 }
